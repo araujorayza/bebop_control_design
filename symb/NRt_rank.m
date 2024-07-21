ffb01974bbc79f80c3705436b7a91e0c7db928ca
araujorayza <47280806+araujorayza@users.noreply.github.com>
2024-07-21 16:46:49 -0300

    studying V and checking of NRt loses rank at any point

---
+++ symb/NRt_rank.m
@@ -0,0 +1,9 @@
+Definitions;
+
+%Lets check invertibility of NRt
+
+detNRt=det(N*R');
+simplify(detNRt)
+
+%For gamma non zero, it's always invertible. since it is square, that means
+%is has full rank
