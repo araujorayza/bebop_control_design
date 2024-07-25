e28d06136c4cce4c0349fcda50c4cb6c5988e806
Rayza Araujo <47280806+araujorayza@users.noreply.github.com>
2024-07-25 14:09:55 -0300

    calculating the min of V at the border of Z using fmincon

---
+++ V_min_opt.m
@@ -0,0 +1,34 @@
+clc;
+clear;
+close all;
+load drone_sys_data.mat
+
+h=cell(4,1);
+
+h{1} = @(psi) -(cos(psi)^2*(sin(2*psi)/2 - 1))/2;
+h{2} = @(psi) (cos(psi)^2*(sin(2*psi)/2 + 1))/2;
+h{3} = @(psi) -sin(psi)^2*(sin(2*psi)/4 - 1/2);
+h{4} = @(psi)  sin(psi)^2*(sin(2*psi)/4 + 1/2);
+
+V = @(x) sum(arrayfun(@(k) x'*h{k}(x(8))*P{k}*x,G));
+
+ub=[1;
+    1;
+    1;
+    1;
+    1;
+    1;
+    1;
+    pi/4];
+lb=-ub;
+
+function [c,ceq] = borderofZ(x,ub,lb)
+    c=[];
+    ceq= prod(arrayfun(@(num) x(num)-lb(num),1:length(lb)))*...
+                prod(arrayfun(@(num) x(num)-ub(num),1:length(ub)));
+end
+
+options = optimoptions('fmincon','Display','iter','Algorithm','sqp');
+
+[x,fval,exitflag,output,lambda,grad,hessian] = ...
+    fmincon(V,lb,[],[],[],[],lb,ub,@(x) borderofZ(x,ub,lb),options)
